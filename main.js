82b473972311383ed311e3dc34755a9a88a3cd01
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-03-27 15:15:18 -0300

    2703

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador")
 for (let i=0; i < botoes.length; i++) {
   botoes [i].onclik = function() {
     for (let j=0; j<botoes.length;j++){
-        botoes[j].classList.remove ("ativo;")
+        botoes[j].classList.remove ("ativo;");
+        textos[j].classList.remove("ativo");
     }
     botoes [i].classList.add("ativo");
-}
+    textos [i].classList.add("ativo");
+  }
 }
