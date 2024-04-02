8646cb6555cb32792510f7be126cb11096e3b2dd
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-04-02 11:34:37 -0300

    0204

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-02-04T00:00:00");
 let tempoAtual = new Date ();
-contadores[0].textContent = "tempoObjetivo1 - tempoAtual";
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
 for (let i=0; i < botoes.length; i++) {
   botoes [i].onclik = function() {
     for (let j=0; j<botoes.length;j++){
